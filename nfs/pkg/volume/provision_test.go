f4a940283f72bb8c66559957eb427b4b70a5c281
Matt Rickard <mrick@google.com>
2017-03-06 14:22:47 -0800

    upgrade to client-go 1.6

--- nfs/pkg/volume/provision_test.go
+++ nfs/pkg/volume/provision_test.go
@@ -29,9 +29,9 @@ import (
 	"testing"
 
 	"github.com/kubernetes-incubator/external-storage/lib/controller"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/pkg/api/resource"
-	"k8s.io/client-go/pkg/api/unversioned"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/runtime"
 	utiltesting "k8s.io/client-go/pkg/util/testing"
@@ -238,7 +238,7 @@ func TestValidateOptions(t *testing.T) {
 		{
 			name: "non-nil selector",
 			options: controller.VolumeOptions{
-				PVC: newClaim(resource.MustParse("1Ki"), nil, &unversioned.LabelSelector{MatchLabels: nil}),
+				PVC: newClaim(resource.MustParse("1Ki"), nil, &meta_v1.LabelSelector{MatchLabels: nil}),
 			},
 			expectedGid: "",
 			expectError: true,
@@ -606,7 +606,7 @@ func TestGetServer(t *testing.T) {
 	}
 }
 
-func newClaim(capacity resource.Quantity, accessmodes []v1.PersistentVolumeAccessMode, selector *unversioned.LabelSelector) *v1.PersistentVolumeClaim {
+func newClaim(capacity resource.Quantity, accessmodes []v1.PersistentVolumeAccessMode, selector *meta_v1.LabelSelector) *v1.PersistentVolumeClaim {
 	claim := &v1.PersistentVolumeClaim{
 		ObjectMeta: v1.ObjectMeta{},
 		Spec: v1.PersistentVolumeClaimSpec{
